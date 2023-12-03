0e9211a239f37a2df9081aa598ef2090fb19a634
Nicole Kelly <nrkelly57@gmail.com>
2023-12-03 13:15:39 +1300

    Updated index.js

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
+const dbConnect = require("./db/dbConnect");
 const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");
 
 app.use(cors());
 
+// Load environment variables from a .env file
+require("dotenv").config();
+
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+// Import your Mongoose model schemas
+const User = require("./models/user");
+const Message = require("./models/message");
+const ChatRoom = require("./models/chatRoom");
+
+// Initialize socket
+const configureSocket = require("./sockets/socketManager");
+
+const io = configureSocket(server);
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+// Use environment variable for the server port
+const PORT = process.env.PORT || 3001;
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected:", socket.id);
-  });
-});
-
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
+// Server listens on the specified port
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
