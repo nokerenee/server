29a1aa661bac9fad49b1b479513838d28f06efb5
nouran-e <elhilali.nouran@gmail.com>
2023-12-06 20:38:56 +1300

    fix for receive message and username things

--- src/sockets/socketManager.js
+++ src/sockets/socketManager.js
@@ -1,6 +1,5 @@
-// sockets/socketManager.js
 const { Server } = require("socket.io");
-const { addUser } = require("../controllers/userController"); // Import user controller or functions for interacting with the database
+const { addUser } = require("../controllers/userController");
 const Message = require("../models/message");
 const { addRoom } = require("../controllers/roomController");
 const { fetchMessagesByRoomId } = require("../controllers/messageController");
@@ -13,68 +12,74 @@ const configureSocket = (server) => {
     },
   });
 
+  const handleJoinRoom = async (socket, data, callback) => {
+    try {
+      const { user, error } = await addUser(data.username);
+      if (error) {
+        console.error("Error adding user:", error);
+        socket.emit("join_error", { error });
+      } else {
+        const room = await addRoom(data.room);
+        console.log('data.room', data.room);
+        socket.join(data.room);
+        console.log(`User with ID: ${socket.id} joined room: ${data.room}`);
+        callback({ user, room });
+        socket.to(data.room).emit("user_joined", { user });
+      }
+    } catch (error) {
+      console.error("Error joining room:", error);
+      socket.emit("join_error", { error: "Internal server error" });
+    }
+  };
+  
+
+  const handleSendMessage = async (socket, data) => {
+    console.log("data", data);
+  
+    try {
+      const newMessage = new Message({
+        message: data.message,
+        sender: data.sender._id,
+        room: data.room._id,
+      });
+      await newMessage.save();
+    } catch (error) {
+      console.error("Error saving new message:", error);
+    }
+  
+    // Broadcast the new message to other clients in the same room
+    console.log(`Broadcasting message to room: ${data.room.name}`);
+    // todo: figure out why this doesn't emit
+    const sockets = await io.in(data.room.name).fetchSockets();
+    // const users = io.sockets.clients(data.room._id);
+    console.log('socketIds',sockets.map(s=>s.id));
+    socket.to(data.room.name).emit("receive_message", data);
+  };
+  
+  const handleGetMessages = async (socket, room, callback) => {
+    try {
+      const messages = await fetchMessagesByRoomId(room._id);
+      callback(messages);
+    } catch (error) {
+      console.error("Error fetching previous messages:", error);
+    }
+  };
+  
+
   io.on("connection", async (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
     socket.on("join_room", async (data, callback) => {
-      try {
-        const { user, error } = await addUser(data.username);
-        if (error) {
-          // Handle error, send an error message to the user, etc.
-          console.log(error);
-          socket.emit("join_error", { error });
-        } else {
-          const room = await addRoom(data.room);
-          socket.join(data.room);
-          console.log(room);
-          console.log(`User with ID: ${socket.id} joined room: ${data}`);
-          // Broadcast to everyone in the room that a new user has joined
-          callback({ user, room });
-          // io.to(data.room).emit("user_joined", { user });
-        }
-      } catch (error) {
-        console.log("Error joining room:", error);
-        socket.emit("join_error", { error: "Internal server error" });
-      }
-
-      // Fetch previous messages from the database for the joined room
-      // try {
-      //   const messages = await Message.find({ room: data }).populate("sender");
-      //   // Send previous messages to the client
-      //   io.to(socket.id).emit("previous_messages", messages);
-      // } catch (error) {
-      //   console.error("Error fetching previous messages:", error);
-      // }
-
-      // console.log(`User with ID: ${socket.id} joined room: ${data}`);
+      await handleJoinRoom(socket, data, callback);
+      callback({ success: true, message: "Successfully joined the room" });
     });
 
     socket.on("send_message", async (data) => {
-      console.log("data", data);
-
-      // Save the new message to the database
-      try {
-        const newMessage = new Message({
-          message: data.message,
-          sender: data.sender._id,
-          room: data.room._id,
-        });
-        await newMessage.save();
-      } catch (error) {
-        console.error("Error saving new message:", error);
-      }
-
-      // Broadcast the new message to other clients in the same room
-      socket.to(data.room).emit("receive_message", data);
+      await handleSendMessage(socket, data);
     });
 
     socket.on("get_messages", async (room, callback) => {
-      try {
-        const messages = await fetchMessagesByRoomId(room._id);
-        callback(messages);
-      } catch (e) {
-        console.log(e);
-      }
+      await handleGetMessages(socket, room, callback);
     });
 
     socket.on("disconnect", () => {
